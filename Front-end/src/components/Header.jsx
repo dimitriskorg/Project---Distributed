46017b7fdba95e56afd5a598ccf28cdb3e33dc5c
Dimitris Korgialas <165560911+dimitriskorg@users.noreply.github.com>
2026-02-05 18:33:32 +0200

    Clean up comments in Header component
    Removed commented sections for clarity and improved code readability.

--- Front-end/src/components/Header.jsx
+++ Front-end/src/components/Header.jsx
@@ -2,12 +2,8 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
-    // 1. Ορισμός μεταβλητών 
     const navigate = useNavigate();
     const [searchText, setSearchText] = useState("");
-
-    // 2. CSS
-    // Header
     const headerStyle = {
         display: 'flex',
         justifyContent: 'space-between',
@@ -17,8 +13,6 @@ export default function Header() {
         color: 'white',
         gap: '15px'
     };
-
-    // Buttons
     const buttonStyle = {
         padding: '10px',
         fontSize: '16px',
@@ -28,8 +22,6 @@ export default function Header() {
         border: 'none',
         borderRadius: '5px',
     };
-
-    // Search field
     const inputStyle = {
         padding: '10px',
         borderRadius: '5px',
@@ -39,24 +31,14 @@ export default function Header() {
 
     return (
         <div style={headerStyle}>
-            {/* Κάναμε το Logo κλικαρισμένο για να πηγαίνει στην Αρχική (/) */}
             <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
                 <h2 style={{ margin: 0 }}>CourseSpark</h2>
             </Link>
-
-            {/* Πρόσθεσα ένα div για να ομαδοποιήσω τα κουμπιά δεξιά */}
             <div style={{ display: 'flex', gap: '10px' }}>
-                
-                {/* 1. Το πεδίο INPUT */}
                 <input type="text" placeholder="Αναζήτηση..." style={inputStyle}
-
-                    // Α. Σύνδεση με το state (Two-way binding)
                     value={searchText}
-                    
-                    // Β. Ενημέρωση του state όταν γράφει ο χρήστης
                     onChange={(e) => setSearchText(e.target.value)}
                 />
-                {/* 2. Σύνδεση με το SearchPage */}
                 <button style={buttonStyle} onClick={()=> navigate(`/search?q=${searchText}`)}> Αναζήτηση </button>
             </div>
         </div>
